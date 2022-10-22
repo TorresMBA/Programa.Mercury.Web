471348ccd7fc9ab5d8238fecdf776865a0e93b31
TorresMBA <bryan98tm@gmail.com>
2022-10-22 01:16:57 -0500

    Se agrega estructura de proyectos y uno encargado para las pruebas unitarias (Unit Test)

---
+++ src/Mercury.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Mercury.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
